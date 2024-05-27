0a3348411551ae38572fa959c5c8795dfb9ade3d
ZhangYiqun018 <2210737@stu.neu.edu.cn>
2024-05-27 23:05:51 +0800

    update bash

--- evaluates/epitome/run.sh
+++ evaluates/epitome/run.sh
@@ -1,4 +1,5 @@
-CUDA_VISIBLE_DEVICES=0 python evaluate.py \
-    --datatype pegs_p \
-    --data_dir /datas/zyq/research/chat_meme/prediction_anno_full.json \
-    --batch_size 4 > nohup_epitome_pegs_p.out 2>&1 &
+TYPE=pegs_v1.5_ret
+CUDA_VISIBLE_DEVICES=4 python evaluates.py \
+    --datatype $TYPE \
+    --data_dir /datas/zyq/research/chat_meme/evaluates/outputs/baseline/pegs_v1.5_ret_full_anno.json \
+    --batch_size 4 > nohup_epitome\_$TYPE.out 2>&1 &
--- evaluates/run.sh
+++ evaluates/run.sh
@@ -1,3 +1,8 @@
-cp -r /datas/zyq/research/chat_meme/evaluates/generate_response_zyq.py /datas/llm_datasets/kfhcode/memechat/generate_response_zyq.py
-CUDA_VISIBLE_DEVICES=6 python /datas/llm_datasets/kfhcode/memechat/generate_response_zyq.py \
-    --data_path /datas/zyq/research/chat_meme/evaluates/temp/tran_converted_test.json > nohup2.out 2>&1 &
+# cp -r /datas/zyq/research/chat_meme/evaluates/generate_response_zyq.py /datas/llm_datasets/kfhcode/memechat/generate_response_zyq.py
+RUNNAME=1\_31\_19
+export HF_ENDPOINT=https://hf-mirror.com
+export CUDA_VISIBLE_DEVICES=2 
+
+python /datas/llm_datasets/memechat/codes/generate_response.py \
+    --data_path /datas/zyq/research/chat_meme/evaluates/util/pegs_1.5_test_v3.json \
+    --output_path /datas/zyq/research/chat_meme/evaluates/outputs/baseline/pegs_retrieve_$RUNNAME.json > nohup\_$RUNNAME.out 2>&1 &
